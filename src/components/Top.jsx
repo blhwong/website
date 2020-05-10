1a1712a1a5a7cd6e942d4c2ce9a9237d1b399081
Brandon Wong <brandon@yerdle.com>
2020-05-10 13:02:59 -0700

    More mobile styling

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -6,8 +6,8 @@ function Top() {
   return (
     <div className="page" id="top">
       <h1>Hi, I&apos;m Brandon Wong</h1>
-      <h3>Software Engineer</h3>
-      <h3>SF, California</h3>
+      <h4>Software Engineer</h4>
+      <h4>SF, California</h4>
     </div>
   );
 }
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -15,44 +15,48 @@ function Contact() {
     <div className="page" id="contact">
       <div className="links">
         <div>
-          <a
-            href={`mailto:${emailAddress}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={email} alt="email logo" />
-          </a>
-          <p>{emailAddress}</p>
+          <div>
+            <a
+              href={`mailto:${emailAddress}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={email} alt="email logo" />
+            </a>
+            <p>{emailAddress}</p>
+          </div>
+          <div>
+            <a
+              href={`https://${githubLink}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={github} alt="" />
+            </a>
+            <p>{githubLink}</p>
+          </div>
         </div>
         <div>
-          <a
-            href={`https://${githubLink}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={github} alt="" />
-          </a>
-          <p>{githubLink}</p>
-        </div>
-        <div>
-          <a
-            href={`https://${linkedinLink}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={linkedin} alt="" />
-          </a>
-          <p>{linkedinLink}</p>
-        </div>
-        <div>
-          <a
-            href={`https://${angellistLink}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={angellist} alt="" />
-          </a>
-          <p>{angellistLink}</p>
+          <div>
+            <a
+              href={`https://${linkedinLink}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedin} alt="" />
+            </a>
+            <p>{linkedinLink}</p>
+          </div>
+          <div>
+            <a
+              href={`https://${angellistLink}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={angellist} alt="" />
+            </a>
+            <p>{angellistLink}</p>
+          </div>
         </div>
       </div>
     </div>
