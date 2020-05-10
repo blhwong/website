094e6f1261536927686271afee891d1cb39cca6e
Brandon Wong <brandon@yerdle.com>
2020-05-09 23:46:28 -0700

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/website',
   siteMetadata: {
     title: 'blhwong',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    description: 'Brandon Wong personal website',
     author: 'Brandon Wong',
   },
   plugins: [
@@ -25,7 +25,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png',
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
